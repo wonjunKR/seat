db326575d68599664bfa465f3e9e94c21e505dbd
JiYoon <wldbs9211@nate.com>
2016-09-24 19:40:47 +0900

    V1.3 그래프 일.월.년 옵션에 맞게 변경 완료

--- app/src/main/java/com/seat/sw_maestro/seat/Tab2.java
+++ app/src/main/java/com/seat/sw_maestro/seat/Tab2.java
@@ -9,6 +9,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
@@ -19,6 +22,7 @@ public class Tab2 extends Fragment {
 
     private static final String TAG = "Tab2";
     String graphType;   // 그래프 표현 방식
+    Spinner spinnerGraphInterval;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,58 +40,121 @@ public class Tab2 extends Fragment {
         else{ // 디폴트는 꺾은선 그래프
             v = inflater.inflate(R.layout.tab_2_line, container, false);
         }
+
         return v;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        GraphManager graphManager = new GraphManager(); // 그래프를 그리기 위한 그래프 매니저
+        final GraphManager graphManager = new GraphManager(); // 그래프를 그리기 위한 그래프 매니저
 
-        // **** 나중에 요 아래 데이터들만 가져오는 함수 구현해서 값만 바꿔주면 되겠지?? ****
-        // 라벨 데이터 만들기
-        String[] labels = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-                "11","12","13","14","15","16","17","18","19","20","21","22","23"};
-        // 인덱스
-        int[] index = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
-        // 앉은 시간 데이터
-        float[] timeDatas = {43,60,0,0,24,0,60,53,20,0,0,0,0,0,0,0,0,0,0,0,4,60,56,43};
-        // 정확도 데이터
-        float[] accuracyDatas = {67,77,0,0,87,0,78,86,86,0,0,0,0,0,0,0,0,0,0,0,76,56,86,78};
+        //스피너 관련
+        spinnerGraphInterval = (Spinner)this.getActivity().findViewById(R.id.spinnerGraphInterval);
+        ArrayAdapter adapterGraphInterval = ArrayAdapter.createFromResource(this.getActivity(), R.array.graphInterval, R.layout.spinner_layout);
+        spinnerGraphInterval.setAdapter(adapterGraphInterval);
+        spinnerGraphInterval.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                //그래프를 일, 월, 연 중에서 어떤 것으로 보여줄지..
+                String graphInterval = spinnerGraphInterval.getSelectedItem().toString();
+                Log.d(TAG, "graphInterval : " + graphInterval);
 
+                String[] labels;
+                int[] index;
+                float[] timeDatas;
+                float[] accuracyDatas;
 
-        switch(graphType){
-            case "0" :  // 꺾은선 그래프
-                LineChart lineChart_time = (LineChart) getView().findViewById(R.id.lineChart_time);
-                LineChart lineChart_accuracy = (LineChart) getView().findViewById(R.id.lineChart_accuracy);
+                switch(graphInterval){
+                    case "일":
+                        // 라벨 데이터 만들기
+                        labels = new String[] {"0시", "", "", "3시", "", "", "6시", "", "", "9시", "",
+                                "","12시","","","15시","","","18시","","","21시","",""};
+                        // 인덱스
+                        index = new int[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
+                        // 앉은 시간 데이터
+                        timeDatas = new float[] {-55,60,0,0,24,0,60,53,20,0,0,0,0,0,0,0,0,0,0,0,4,60,56,43};
+                        // 정확도 데이터
+                        accuracyDatas = new float[] {67,77,0,0,87,0,78,86,86,0,0,0,0,0,0,0,0,0,0,0,76,56,86,78};
 
-                // 앉은 시간 그래프
-                // 그래프 매니저를 통해서 쉽게 그래프를 위한 데이터 셋을 만들 수 있다.
-                LineData lineChart_time_data = graphManager.makeLineData(timeDatas,index,labels,"time"); // 만든 데이터 셋
-                lineChart_time.setData(lineChart_time_data); // 만든 데이터 셋을 차트에만 연결해주면 됨.
-                lineChart_time.animateY(5000);
+                        break;
 
-                // 정확도 그래프
-                LineData lineChart_accuracy_data = graphManager.makeLineData(accuracyDatas,index,labels,"accuracy"); // 만든 데이터 셋
-                lineChart_accuracy.setData(lineChart_accuracy_data);
-                lineChart_accuracy.animateY(5000);
-                break;
+                    case "월":
+                        // 라벨 데이터 만들기
+                        labels = new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                                "11","12","13","14","15","16","17","18","19","20","21","22","23",
+                                "24","25","26","27","28","29","30","31"};
+                        // 인덱스
+                        index = new int[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+                        // 앉은 시간 데이터
+                        timeDatas = new float[] {-55,60,0,0,24,0,60,53,20,0,-55,60,0,0,24,0,60,53,20,0,-55,60,0,0,24,0,60,53,20,0,2};
+                        // 정확도 데이터
+                        accuracyDatas = new float[] {67,77,0,0,87,0,78,86,86,67,77,0,0,87,0,78,86,86,67,77,0,0,87,0,78,86,86,1,56,75,84};
+                        break;
 
-            case "1" :
-                BarChart barChart_time = (BarChart) getView().findViewById(R.id.barChart_time);
-                BarChart barChart_accuracy = (BarChart) getView().findViewById(R.id.barChart_accuracy);
+                    case "연":
+                        // 라벨 데이터 만들기
+                        labels = new String[] {"1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월",
+                                "11월","12월"};
+                        // 인덱스
+                        index = new int[] {0,1,2,3,4,5,6,7,8,9,10,11};
+                        // 앉은 시간 데이터
+                        timeDatas = new float[] {-55,60,0,0,24,0,60,53,20,0,0,0};
+                        // 정확도 데이터
+                        accuracyDatas = new float[] {67,77,0,0,87,0,78,86,86,0,0,0,0};
+                        break;
 
-                // 앉은 시간 그래프
-                // 그래프 매니저를 통해서 쉽게 그래프를 위한 데이터 셋을 만들 수 있다.
-                BarData barChart_time_data = graphManager.makeBarData(timeDatas,index,labels,"time"); // 만든 데이터 셋
-                barChart_time.setData(barChart_time_data);
-                barChart_time.animateY(5000);
+                    default :
+                        // 라벨 데이터 만들기
+                        labels = new String[] {"0"};
+                        // 인덱스
+                        index = new int[] {0};
+                        // 앉은 시간 데이터
+                        timeDatas = new float[] {0};
+                        // 정확도 데이터
+                        accuracyDatas = new float[] {0};
+                        break;
+                }
 
-                // 정확도 그래프
-                BarData barChart_accuracy_data = graphManager.makeBarData(accuracyDatas,index,labels,"accuracy"); // 만든 데이터 셋
-                barChart_accuracy.setData(barChart_accuracy_data);
-                barChart_accuracy.animateY(5000);
-                break;
-        }
+                // 설정의 그래프 타입에 맞게 그리기
+                switch(graphType){
+                    case "0" :  // 꺾은선 그래프
+                        LineChart lineChart_time = (LineChart) getView().findViewById(R.id.lineChart_time);
+                        LineChart lineChart_accuracy = (LineChart) getView().findViewById(R.id.lineChart_accuracy);
+
+                        // 앉은 시간 그래프
+                        // 그래프 매니저를 통해서 쉽게 그래프를 위한 데이터 셋을 만들 수 있다.
+                        LineData lineChart_time_data = graphManager.makeLineData(timeDatas,index,labels,"time"); // 만든 데이터 셋
+                        lineChart_time.setData(lineChart_time_data); // 만든 데이터 셋을 차트에만 연결해주면 됨.
+                        lineChart_time.animateY(5000);
+
+                        // 정확도 그래프
+                        LineData lineChart_accuracy_data = graphManager.makeLineData(accuracyDatas,index,labels,"accuracy"); // 만든 데이터 셋
+                        lineChart_accuracy.setData(lineChart_accuracy_data);
+                        lineChart_accuracy.animateY(5000);
+                        break;
+
+                    case "1" :  // 막대 그래프
+                        BarChart barChart_time = (BarChart) getView().findViewById(R.id.barChart_time);
+                        BarChart barChart_accuracy = (BarChart) getView().findViewById(R.id.barChart_accuracy);
+
+                        // 앉은 시간 그래프
+                        // 그래프 매니저를 통해서 쉽게 그래프를 위한 데이터 셋을 만들 수 있다.
+                        BarData barChart_time_data = graphManager.makeBarData(timeDatas,index,labels,"time"); // 만든 데이터 셋
+                        barChart_time.setData(barChart_time_data);
+                        barChart_time.animateY(5000);
+
+                        // 정확도 그래프
+                        BarData barChart_accuracy_data = graphManager.makeBarData(accuracyDatas,index,labels,"accuracy"); // 만든 데이터 셋
+                        barChart_accuracy.setData(barChart_accuracy_data);
+                        barChart_accuracy.animateY(5000);
+                        break;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                Log.d(TAG, "아무것도 고르지 않음");
+            }
+        });
         super.onActivityCreated(savedInstanceState);
         // 뷰에 데이터를 넣는 작업 등을 할 추가할 수 있음
     }
@@ -95,7 +162,6 @@ public class Tab2 extends Fragment {
     @Override
     public void onStart() {
         Log.d(TAG, "onStart()");
-        // 이때 세팅 이후 그래프의 모양과 관련된 것을 바꾸면 되겠지?
         super.onStart();
     }
 }
