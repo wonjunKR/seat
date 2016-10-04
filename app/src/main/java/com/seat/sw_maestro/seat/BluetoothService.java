a86c1850670a44bd6acbcbebc822bff24e2ae2a1
JiYoon <wldbs9211@nate.com>
2016-10-04 19:08:50 +0900

    V1.5.6 블루투스 연결 상태 관련하여 수정 기능추가 및 변경 1. 블루투스 서비스 클래스 (방석 연결 상태 표시)   1.1 메시지 관련 추가. 블루투스 상태에 따라서 메시지로 Tab1 Activity로 전달. 방석의 블루투스 상태 알려줌 2. 요약 fragment xml 수정 등 잡다한 것
    해결이 필요한 문제 리스트
    1. 블루투스 등록에 핀 번호 입력 없앨 수 있나?
    2. 연결 중에 폰에서 블루투스 꺼버리면 죽는다.(??? 가끔 그러고 테스트 더 필요)
    3. 서비스가 여러 상황에서도 자동으로 되살아나는지 확인이 필요함
    4. 방석 연결 상태가 방석 전원 끄고 키는거에는 정상작동. 멀리 벗어나서 연결 놓치는 것은 테스트 중...

--- app/src/main/java/com/seat/sw_maestro/seat/BluetoothService.java
+++ app/src/main/java/com/seat/sw_maestro/seat/BluetoothService.java
@@ -2,7 +2,11 @@ package com.seat.sw_maestro.seat;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
@@ -13,20 +17,57 @@ public class BluetoothService extends Service {
     private static final String TAG = "BluetoothService";
     private static final String SeatName = "wonjun";    // 방석의 블루투스 이름을 입력한다.
     BluetoothSPP bt;
+    private Messenger mRemote;  // 서비스와 액티비티 간에 통신을 하기 위해서 쓰는 메신저
+
+    // 생성자
+    BluetoothService(){
+
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return new Messenger(new RemoteHandler()).getBinder();
+    }
+
+    public void remoteSendMessage(String data) {    // 액티비티로 메시지 전달. 방석의 연결 유무 상태를 나타내주기 위해 사용
+        if (mRemote != null) {
+            Message msg = new Message();
+            msg.what = 1;
+            msg.obj = data; // 오브젝트로 String data를 보낸다.
+            try {
+                mRemote.send(msg);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Service handler 추가
+    private class RemoteHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) { // 메시지의 what의 타입에 따라서 보내는게 달라진다. 여러 액티비티에 보내야하는 경우에는 나눠야겠지?
+                case 0 :    // 0은 방석 연결 유무용
+                    // Register activity hander
+                    mRemote = (Messenger) msg.obj;
+                    break;
+                default :
+                    remoteSendMessage("TEST");
+                    break;
+            }
+        }
     }
 
     @Override
     public void onCreate() {
         bt = new BluetoothSPP(getApplicationContext());
 
+        // 자동연결부분
         bt.setupService();
         bt.startService(BluetoothState.DEVICE_OTHER);
         bt.autoConnect(SeatName);
 
+        // 리스너
         bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
             public void onDataReceived(byte[] data, String message) {
                 // Do something when data incoming
@@ -35,19 +76,24 @@ public class BluetoothService extends Service {
             }
         });
 
+        // 블루투스 상태 리스너
         bt.setBluetoothStateListener(new BluetoothSPP.BluetoothStateListener() {
             public void onServiceStateChanged(int state) {
                 if(state == BluetoothState.STATE_CONNECTED){
                     Log.d(TAG, "연결된 상태");
+                    remoteSendMessage("1");
                 }
                 else if(state == BluetoothState.STATE_CONNECTING) {
                     Log.d(TAG, "연결중인 상태");
+                    remoteSendMessage("0");
                 }
                 else if(state == BluetoothState.STATE_LISTEN) {
                     Log.d(TAG, "리슨 상태");
+                    remoteSendMessage("0");
                 }
                 else if(state == BluetoothState.STATE_NONE) {
-                    Log.d(TAG, "아무상태도아님");
+                    Log.d(TAG, "아무것도 아닌 상태");
+                    remoteSendMessage("0");
                 }
             }
         });
--- app/src/main/java/com/seat/sw_maestro/seat/Tab1.java
+++ app/src/main/java/com/seat/sw_maestro/seat/Tab1.java
@@ -1,32 +1,94 @@
 package com.seat.sw_maestro.seat;
 
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Tab1 extends Fragment {
 
     private static final String TAG = "Tab1";
+    private Messenger mRemote;  // 블루투스 서비스로부터 받아오는 메시지. 블루투스 연결 상태를 확인하기 위해서
+
+    TextView textView_bluetoothState;
+
+    private ServiceConnection mConnection = new ServiceConnection() {   // 서비스와 핸들러를 연결해주는 부분
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mRemote = new Messenger(service);   // service 하고 연결될때
+
+            if (mRemote != null) {  // Activity handler를 service에 전달하기
+                Message msg = new Message();    // 새로운 메시지를 만들고
+                msg.what = 0;
+                msg.obj = new Messenger(new RemoteHandler());   // 액티비티의 핸들러를 전달한다.
+                try {
+                    mRemote.send(msg);  // 전달
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mRemote = null; // service 하고 연결이 끊길때
+        }
+    };
+
+    // 서비스로부터 메시지 받았을 때 어떻게 처리할 것인가? - 우리는 방석의 블루투스 연결 상태를 바꿔준다.
+    private class RemoteHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {    // 핸들러 메시지로는 1이면 연결상태, 0이면 연결안된 상태가 들어온다.
+            if(msg.obj.toString() == "1")
+                changeBluetoothState(1);
+            else
+                changeBluetoothState(0);
+            super.handleMessage(msg);
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.tab_1,container,false);
+
+
         return v;
 
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
 
+        textView_bluetoothState = (TextView) getActivity().findViewById(R.id.bluetoothState);  // 블루투스 연결 상태를 보여주는 텍스트뷰
+        // service 연결 시도
+        Intent serviceIntent = new Intent(getContext(), BluetoothService.class);
+        getActivity().bindService(serviceIntent, mConnection, Context.BIND_AUTO_CREATE);
+
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    public void changeBluetoothState(int state){    // 1이면 연결된 상태, 다른 값은 연결되지 않은 상태로 한다.
+        if(state == 1)
+            textView_bluetoothState.setText("방석이 연결되었습니다.");
+        else
+            textView_bluetoothState.setText("방석이 연결되지 않았습니다.");
     }
 }
